14e97f77ce9ff377af3a4d00469840fe684b0079
Stefanos Gatsios <stevengatsios@gmail.com>
2016-11-23 17:53:40 +0200

    Early version of forms without composing

--- app/pages/resourceregistration/component/component-registration-form.component.ts
+++ app/pages/resourceregistration/component/component-registration-form.component.ts
@@ -1,20 +1,36 @@
 /**
  * Created by stefania on 10/19/16.
  */
-import { Component } from '@angular/core';
-import {MetadataHeaderInfo, OMTDCorpus} from "../../../domain/openminted-model";
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms'
+import {MetadataHeaderInfo, OMTDComponent} from "../../../domain/openminted-model";
+import {MetadataHeaderInfoFormControl} from "../shared/metadata-header-info-form.component";
 
 @Component({
     selector: 'component-registration-form',
     templateUrl: 'app/pages/resourceregistration/component/component-registration-form.component.html',
-    // styleUrls:  ['app/pages/resourceregistration/component/component-registration-form.component.css'],
 })
 
-export class ComponentRegistrationFormComponent {
-    
-    corpus: OMTDCorpus;
+export class ComponentRegistrationFormComponent implements OnInit {
 
-    constructor() {
-        console.log(MetadataHeaderInfo.prototype.label);
+    component: OMTDComponent;
+
+    metadataHeaderInfo: MetadataHeaderInfoFormControl;
+    myForm: FormGroup;
+
+    constructor(private _fb: FormBuilder) {
+        this.metadataHeaderInfo = new MetadataHeaderInfoFormControl(_fb);
     }
+
+    ngOnInit() {
+        this.myForm = this._fb.group({
+            value: this.metadataHeaderInfo.formGroup
+
+        });
+    }
+
+    onSubmit(myForm: any) {
+        console.log(myForm);
+    }
+
 }
