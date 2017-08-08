52d2a99d6d31c319f0d6202addf5cbca1f6b5e63
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-08-08 20:12:01 +0300

    Housekeeping

--- app/services/user.service.ts
+++ app/services/user.service.ts
@@ -1,33 +1,28 @@
 /**
  * Created by stefania on 8/30/16.
  */
-import { Injectable } from '@angular/core';
-import { Http, Response, Headers, RequestOptions } from '@angular/http';
-import { User } from './../domain/user';
-import { Observable } from 'rxjs/Rx';
+import {Injectable} from "@angular/core";
+import {Headers, Http, RequestOptions, Response} from "@angular/http";
+import {User} from "../domain/eic-model";
+import {Observable} from "rxjs/Rx";
 
 @Injectable()
 export class UserService {
 
-    constructor (private http: Http) {}
-
-    private _usersUrl = 'http://83.212.98.33:8080/omtd-registry/user/';
+    constructor(private http: Http) {
+    }
 
     loginUser(username: string, password: string): Observable<User> {
-
-        return this.http.get(this._usersUrl + 'login/' + username + '/' + password)
+        return this.http.post(process.env.API_ENDPOINT + "/user/login", JSON.stringify({username, password}))
             .map(res => <User> res.json())
             // .map(this.extractData)
             .catch(this.handleError);
     }
 
     registerUser(user: User): Observable<User> {
+        let args = new RequestOptions({headers: new Headers({"Content-Type": "application/json"})});
 
-        let body = JSON.stringify({ user });
-        let headers = new Headers({ 'Content-Type': 'application/json' });
-        let options = new RequestOptions({ headers: headers });
-
-        return this.http.post(this._usersUrl + 'register/', body, options)
+        return this.http.post(process.env.API_ENDPOINT + "/user/register", JSON.stringify({user}), args)
             .map(res => <User> res.json())
             // .map(this.extractData)
             .catch(this.handleError);
@@ -35,14 +30,11 @@ export class UserService {
 
     private extractData(res: Response) {
         let body = res.json();
-        return body.data || { };
+        return body.data || {};
     }
 
-    private handleError (error: any) {
-        // In a real world app, we might use a remote logging infrastructure
-        // We'd also dig deeper into the error to get a better message
-        let errMsg = (error.message) ? error.message :
-            error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    private handleError(error: any) {
+        let errMsg = (error.message) ? error.message : error.status ? `${error.status} - ${error.statusText}` : "Server error";
         console.error(errMsg); // log to console instead
         return Observable.throw(errMsg);
     }
