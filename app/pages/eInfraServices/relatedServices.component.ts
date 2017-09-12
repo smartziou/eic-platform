84e20777ef3f90af94a2ad0fe5f9f9095e69028d
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-09-12 18:50:06 +0300

    Transforming via Array.prototype.reduce now

--- app/pages/eInfraServices/relatedServices.component.ts
+++ app/pages/eInfraServices/relatedServices.component.ts
@@ -1,15 +1,13 @@
-import {Component, Injector} from '@angular/core';
+import {Component, Injector} from "@angular/core";
 import {MyGroup} from "../multiforms/my-group.interface";
 import * as sd from "./services.description";
 import {Validators} from "@angular/forms";
 import {ResourceService} from "../../services/resource.service";
-import {injectorFactory} from "@angular/upgrade/static/src/static/angular1_providers";
-import {transform} from "../../domain/utils";
 // import {KeysPipe} from "../../services/key.pipe";
 
 @Component({
     selector: 'relatedServicesInfo-form',
-    template : `
+    template: `
         <div [formGroup]="group">
             <select formControlName="entry">
                 <option *ngFor="let c of relatedServices | keys" [ngValue]="c">{{relatedServices[c]}}</option>
@@ -20,33 +18,33 @@ import {transform} from "../../domain/utils";
 
 export class RelatedServicesComponent extends MyGroup {
 
-    constructor(private resourceService: ResourceService, injector : Injector) {
+    constructor(private resourceService: ResourceService, injector: Injector) {
         super(injector);
     }
 
-    relatedServices : any = {
+    relatedServices: any = {
         "00.00": "Failed to fetch services"
     };
 
     readonly groupDefinition = {
-        entry : ["", Validators.required]
+        entry: ["", Validators.required]
     };
 
-    readonly relatedServicesDesc : sd.Description = sd.relatedServicesDesc;
+    readonly relatedServicesDesc: sd.Description = sd.relatedServicesDesc;
 
     ngOnInit() {
         super.ngOnInit();
         this.resourceService.getServices().subscribe(
-            suc=> this.relatedServices = this.transformInput(suc),
+            suc => this.relatedServices = this.transformInput(suc),
             err => console.error(err)
         );
     }
 
     transformInput(input) {
-        Object.keys(input).forEach(k => {
-            input[k] = input[k][0].provider + " - " + input[k][0].brandName;
-        });
-        return input;
+        return Object.keys(input).reduce((accumulator, value) => {
+            console.log(value);
+            accumulator[value] = input[value][0].provider + " - " + input[value][0].brandName;
+            return accumulator;
+        }, {});
     }
-
 }
