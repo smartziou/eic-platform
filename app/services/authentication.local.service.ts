14367c432763841220bf08348c50ab361c8375b7
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-09-14 18:43:13 +0300

    Added a local auth service

---
+++ app/services/authentication.local.service.ts
@@ -0,0 +1,31 @@
+/**
+ * Created by pgl on 14/09/17.
+ */
+
+import {Injectable} from "@angular/core";
+import {deleteCookie, getCookie} from "../domain/utils";
+import {Router} from "@angular/router";
+import {Http} from "@angular/http";
+
+@Injectable()
+export class AuthenticationLocalService {
+
+    private endpoint = process.env.API_ENDPOINT;
+
+    constructor(private http: Http, private router: Router) {
+    }
+
+    logout() {
+        deleteCookie("jwt");
+        this.router.navigate(['/home']);
+    }
+
+
+    public getUser(): string {
+        return getCookie('jwt');
+    }
+
+    public loggedIn(): string {
+        return getUser() != null;
+    }
+}
