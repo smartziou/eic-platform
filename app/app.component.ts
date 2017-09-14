20695306dd9868d93273bc8f7cb19862f218af25
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-09-14 18:45:48 +0300

    No longer using oauth

--- app/app.component.ts
+++ app/app.component.ts
@@ -16,20 +16,20 @@ export class AppComponent implements OnInit {
     private isLoginOrRegister: boolean = false;
 
     constructor(private router: Router,private oauthService: AuthenticationService) {
-        // URL of the SPA to redirect the user to after login
-        this.oauthService.redirectUri = window.location.origin + "/home";
-
-        // The SPA's id. The SPA is registerd with this id at the auth-server
-        this.oauthService.clientId = "dfd9f71e-2d7e-41a7-a9c5-bc27cc815868";
-
-        // set the scope for the permissions the client should request
-        // The first three are defined by OIDC. The 4th is a usecase-specific one
-        this.oauthService.scope = "openid";
-
-        // The name of the auth-server that has to be mentioned within the token
-        this.oauthService.loginUrl = "https://aai.openminted.eu/oidc/authorize";
-
-        this.oauthService.tryLogin();
+        // // URL of the SPA to redirect the user to after login
+        // this.oauthService.redirectUri = window.location.origin + "/home";
+        //
+        // // The SPA's id. The SPA is registerd with this id at the auth-server
+        // this.oauthService.clientId = "dfd9f71e-2d7e-41a7-a9c5-bc27cc815868";
+        //
+        // // set the scope for the permissions the client should request
+        // // The first three are defined by OIDC. The 4th is a usecase-specific one
+        // this.oauthService.scope = "openid";
+        //
+        // // The name of the auth-server that has to be mentioned within the token
+        // this.oauthService.loginUrl = "https://aai.openminted.eu/oidc/authorize";
+        //
+        // this.oauthService.tryLogin();
 
     }
 
