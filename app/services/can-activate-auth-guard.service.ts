891349fe7b5ac571570e10020ffd072359a92697
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-09-14 19:14:26 +0300

    Now using local auth

--- app/services/can-activate-auth-guard.service.ts
+++ app/services/can-activate-auth-guard.service.ts
@@ -3,21 +3,20 @@
  */
 import { Injectable } from '@angular/core';
 import { CanActivate, Router, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
-import { AuthenticationService } from "./authentication.service";
-import {getCookie} from "../domain/utils";
+import {AuthenticationLocalService} from "./authentication.local.service";
 
 
 @Injectable()
 export class CanActivateViaAuthGuard implements CanActivate {
 
-    constructor(private authenticationService: AuthenticationService, private router: Router) {}
+    constructor(private authenticationLocalService: AuthenticationLocalService, private router: Router) {}
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
 
-        if (this.authenticationService.isUserLoggedIn()) { return true; }
+        if (this.authenticationLocalService.loggedIn()) { return true; }
 
         // Store the attempted URL for redirecting
-        this.authenticationService.redirectUrl = state.url;
+        //this.authenticationService.redirectUrl = state.url;
 
         // Navigate to the login page
         this.router.navigate(['/signIn']);
