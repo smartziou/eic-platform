b59bb187524c033e4e1f27c47272180cfa0c9c92
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-10-25 20:52:09 +0300

    Added category description array, to generate the categories in /home

--- app/pages/home/home.component.ts
+++ app/pages/home/home.component.ts
@@ -18,6 +18,22 @@ export class HomeComponent {
     public searchForm: FormGroup;
 
     private services: Service[];
+    public categories: category[] = [
+        {
+            value: "Authentication and Authorization Infrastructure",
+            icon: "fingerprint_scanning_authorization.svg",
+            hover: "fingerprint_scanning_authorization_hover.svg"
+        },
+        {value: "Compute", icon: "cloud_server.svg", hover: "cloud_server_hover.svg"},
+        {value: "Connectivity", icon: "connectivity.svg", hover: "connectivity_hover.svg"},
+        {value: "Consulting", icon: "consulting.svg", hover: "consulting_hover.svg"},
+        {value: "Content Delivery", icon: "keyword.svg", hover: "keyword_hover.svg"},
+        {value: "Data Connectivity", icon: "data-connectivity.svg", hover: "data-connectivity_hover.svg"},
+        {value: "Data Discovery", icon: "keyword.svg", hover: "keyword_hover.svg"},
+        {value: "Data Movement", icon: "database.svg", hover: "database_hover.svg"},
+        {value: "Data Registration", icon: "cloud_server.svg", hover: "cloud_server_hover.svg"},
+        {value: "Data Storage", icon: "database_security.svg", hover: "database_security_hover.svg"}
+    ];
 
     constructor(fb: FormBuilder, private router: Router) {
         this.searchForm = fb.group({
