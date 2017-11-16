a0f4e98f50eebe2e147ae6031482d871a5d72782
Panagiotis Lampropoulos <pgl@otenet.gr>
2017-11-16 08:02:08 +0200

    redirect user to their provider's search page, if they exist

--- app/pages/user/dashboard/dashboard.component.ts
+++ app/pages/user/dashboard/dashboard.component.ts
@@ -2,7 +2,9 @@
  * Created by pgl on 28/08/17.
  */
 import {Component, OnInit} from "@angular/core";
+import {Router} from "@angular/router";
 import {AuthenticationService} from "../../../services/authentication.service";
+import {ResourceService} from "../../../services/resource.service";
 import {UserService} from "../../../services/user.service";
 
 @Component({
@@ -11,9 +13,20 @@ import {UserService} from "../../../services/user.service";
     styleUrls: ["./dashboard.component.css"]
 })
 export class DashboardComponent implements OnInit {
-    constructor(public authenticationService: AuthenticationService, public userService: UserService) {
+    constructor(protected authenticationService: AuthenticationService, protected userService: UserService,
+                protected resourceService: ResourceService, protected router: Router) {
     }
 
     ngOnInit() {
+        this.resourceService.getProviders().subscribe(
+            suc => {
+                for (let provider in suc) {
+                    if (this.userService.user.email === provider + "@eic") {
+                        this.router.navigate(["/search", {provider: provider}]);
+                        break;
+                    }
+                }
+            }
+        );
     }
 }
