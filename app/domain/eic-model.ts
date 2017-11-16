7e0e4edd28ad0ae8dac0fef3825ef55329f743d3
Panagiotis Lampropoulos <pgl@otenet.gr>
2017-11-16 21:29:58 +0200

    +v0.95 model.ts

--- app/domain/eic-model.ts
+++ app/domain/eic-model.ts
@@ -1,127 +1,135 @@
-// Generated using typescript-generator version 1.27.339 on 2017-11-03 17:19:28.
-export interface Identifiable {
+// Generated using typescript-generator version 1.27.339 on 2017-11-14 17:15:27.
+
+export class Access implements Identifiable {
     id: string;
+    instant: number;
+    serviceID: string;
+    type: string;
+    userID: string;
+}
+
+export class Dimension {
+}
+
+export class Grant implements Identifiable {
+    description: string;
+    id: string;
+}
+
+export interface Identifiable {
+}
+
+export class Indicator implements Identifiable {
+    description: string;
+    dimensions: Dimension[];
+    id: string;
+    unit: Unit;
+}
+
+export class Measurement<T> implements Identifiable {
+    id: string;
+    indicator: Indicator;
+    location: string[];
+    time: XMLGregorianCalendar;
+    value: any;
+}
+
+export class Membership implements Identifiable {
+    grant: Grant;
+    id: string;
+    provider: Provider;
+    user: User;
 }
 
 export class Provider implements Identifiable {
+    contactInformation: string;
     id: string;
     name: string;
-    contactInformation: string;
-    user: User[];
     service: User[];
+    user: User[];
 }
 
 export class Service implements Identifiable {
-    id: string;
-    url: URL;
-    name: string;
-    tagline: string;
-    description: string;
-    options: string;
-    targetUsers: string;
-    userValue: string;
-    userBase: string;
-    symbol: URL;
-    multimediaURL: URL;
-    provider: string[];
-    version: string;
-    lastUpdate: XMLGregorianCalendar;
-    changeLog: string;
-    validFor: XMLGregorianCalendar;
-    lifeCycleStatus: string;
-    trl: string;
     category: string;
-    subcategory: string;
-    place: string[];
+    changeLog: string;
+    description: string;
+    feedback: URL;
+    funding: string;
+    helpdesk: URL;
+    id: string;
     language: string[];
-    tag: string[];
-    requiredService: string[];
+    lastUpdate: XMLGregorianCalendar;
+    lifeCycleStatus: string;
+    multimediaURL: URL;
+    name: string;
+    options: string;
+    place: string[];
+    price: URL;
+    provider: string[];
     relatedService: string[];
     request: URL;
-    helpdesk: URL;
-    userManual: URL;
-    trainingInformation: URL;
-    feedback: URL;
-    price: URL;
-    serviceLevelAgreement: URL;
-    termOfUse: URL[];
-    funding: string;
+    requiredService: string[];
     serviceAddenda: ServiceAddenda;
+    serviceLevelAgreement: URL;
+    subcategory: string;
+    symbol: URL;
+    tag: string[];
+    tagline: string;
+    targetUsers: string;
+    termOfUse: URL[];
+    trainingInformation: URL;
+    trl: string;
+    url: URL;
+    userBase: string;
+    userManual: URL;
+    userValue: string;
+    validFor: XMLGregorianCalendar;
+    version: string;
 }
 
 export class ServiceAddenda implements Identifiable {
-    id: string;
-    perfomanceData: Measurement<any>[];
-    externalHits: number;
-    internalHits: number;
-    favouriteCount: number;
     averageRating: number;
-    ratings: number;
+    externalHits: number;
+    favouriteCount: number;
+    id: string;
+    internalHits: number;
+    perfomanceData: Measurement<any>[];
     published: boolean;
+    ratings: number;
+}
+
+export class Unit {
+}
+
+export class User implements Identifiable {
+    email: string;
+    favourite: Service[];
+    id: string;
+    iterationCount: number;
+    joinDate: string;
+    membership: Membership[];
+    name: string;
+    password: string;
+    resetToken: string;
+    salt: any;
+    surname: string;
 }
 
 export class Vocabulary implements Identifiable {
     id: string;
     name: string;
-    type: string;
     parent: string;
-}
-
-export class WrappedComparable {
-}
-
-export class Access implements Identifiable {
-    id: string;
-    instant: number;
     type: string;
-    userID: string;
-    serviceID: string;
-}
-
-export class Grant implements Identifiable {
-    id: string;
-    description: string;
-}
-
-export class User implements Identifiable {
-    id: string;
-    email: string;
-    password: string;
-    name: string;
-    surname: string;
-    joinDate: string;
-    membership: { [index: string]: Grant };
-    favourite: Service[];
-    iterationCount: number;
-    salt: any;
-    resetToken: string;
-}
-
-export class Indicator implements Identifiable {
-    id: string;
-    description: string;
-}
-
-export class Measurement<T> implements Identifiable {
-    id: string;
-    indicator: string;
-    from: XMLGregorianCalendar;
-    to: XMLGregorianCalendar;
-    value: T;
-}
-
-export class Target {
-}
-
-export class URL implements Serializable {
 }
 
 export class XMLGregorianCalendar implements Cloneable {
 }
 
-export interface Serializable {
+export class URL implements Serializable {
 }
 
 export interface Cloneable {
 }
 
+export interface Serializable {
+}
