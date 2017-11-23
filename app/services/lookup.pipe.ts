1f0193d69f35f93bc4a12741befef3b7e2fe0141
Panagiotis Lampropoulos <pgl@otenet.gr>
2017-11-23 19:00:41 +0200

    made lookup more robust

--- app/services/lookup.pipe.ts
+++ app/services/lookup.pipe.ts
@@ -6,6 +6,6 @@ import {Pipe, PipeTransform} from "@angular/core";
 @Pipe({name: "lookup"})
 export class LookUpPipe implements PipeTransform {
     transform(keys: any[], dictionary: any): any {
-        return (keys || []).map(e => dictionary[e]);
+        return (keys || []).map(e => dictionary[e] || e);
     }
 }
