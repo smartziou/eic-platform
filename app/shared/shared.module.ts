78f5e1df60662c0051c4bfa8394ffaa371817e7c
Panagiotis Lampropoulos <pgl@otenet.gr>
2017-11-23 18:38:52 +0200

    +joinpipe

--- app/shared/shared.module.ts
+++ app/shared/shared.module.ts
@@ -2,16 +2,19 @@
  * Created by pgl on 29/09/17.
  */
 import {NgModule} from "@angular/core";
+import {JoinPipe} from "../services/join.pipe";
 import {KeysPipe} from "../services/key.pipe";
 import {PrettifyWithDictionaryPipe} from "../services/prettifyWithDictionary.pipe";
 
 @NgModule({
     imports: [],
     declarations: [
+        JoinPipe,
         KeysPipe,
         PrettifyWithDictionaryPipe
     ],
     exports: [
+        JoinPipe,
         KeysPipe,
         PrettifyWithDictionaryPipe
     ]
