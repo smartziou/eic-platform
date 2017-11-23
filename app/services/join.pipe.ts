4a3b5a7e541bab62a513881a1fed61ccb7dee21b
Panagiotis Lampropoulos <pgl@otenet.gr>
2017-11-23 19:00:33 +0200

    made join more robust

--- app/services/join.pipe.ts
+++ app/services/join.pipe.ts
@@ -6,6 +6,6 @@ import {Pipe, PipeTransform} from "@angular/core";
 @Pipe({name: "join"})
 export class JoinPipe implements PipeTransform {
     transform(arr: any[], separator?: string): any {
-        return arr.join(separator);
+        return (arr || []).join(separator || "");
     }
 }
