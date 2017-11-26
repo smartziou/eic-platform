1723d0f8a14e087de23e9cc6b68713a01f958bf3
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-11-26 21:37:54 +0200

    beaut

--- app/pages/eInfraServices/places.component.ts
+++ app/pages/eInfraServices/places.component.ts
@@ -18,7 +18,7 @@ export class PlacesComponent extends MyGroup {
     places: any = {"QQ": "Error fetching places"};
     readonly groupDefinition = {entry: ["", Validators.required]};
     readonly placesDesc: sd.Description = sd.placesDesc;
-    weights:string[] = ["EU", "WW"];
+    weights: string[] = ["EU", "WW"];
 
     constructor(private resourceService: ResourceService, injector: Injector) {
         super(injector);
