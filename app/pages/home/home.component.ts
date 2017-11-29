45250bed48a2d76dc09ea7b87bd0d7eedd73b856
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-11-29 10:35:52 +0200

    Fixed home browsing categories

--- app/pages/home/home.component.ts
+++ app/pages/home/home.component.ts
@@ -1,7 +1,6 @@
 /**
  * Created by stefania on 7/5/16.
  */
-
 import {Component, OnInit} from "@angular/core";
 import {FormBuilder, FormGroup} from "@angular/forms";
 import {Service} from "../../domain/eic-model";
@@ -17,19 +16,19 @@ export class HomeComponent implements OnInit {
     public searchForm: FormGroup;
     public categories: category[] = [
         {
-            value: "Authentication and Authorization Infrastructure",
+            value: "Authentication and authorization infrastructure",
             icon: "fingerprint_scanning_authorization.svg",
             hover: "fingerprint_scanning_authorization_hover.svg"
         },
         {value: "Compute", icon: "cloud_server.svg", hover: "cloud_server_hover.svg"},
         {value: "Connectivity", icon: "connectivity.svg", hover: "connectivity_hover.svg"},
         {value: "Consulting", icon: "consulting.svg", hover: "consulting_hover.svg"},
-        {value: "Operations", icon: "keyword.svg", hover: "keyword_hover.svg"}
-        // {value: "Data Connectivity", icon: "data-connectivity.svg", hover: "data-connectivity_hover.svg"},
-        // {value: "Data Discovery", icon: "keyword.svg", hover: "keyword_hover.svg"},
-        // {value: "Data Movement", icon: "database.svg", hover: "database_hover.svg"},
-        // {value: "Data Registration", icon: "cloud_server.svg", hover: "cloud_server_hover.svg"},
-        // {value: "Data Storage", icon: "database_security.svg", hover: "database_security_hover.svg"}
+        {value: "Operations", icon: "keyword.svg", hover: "keyword_hover.svg"},
+        {value: "Content delivery", icon: "data-connectivity.svg", hover: "data-connectivity_hover.svg"},
+        {value: "Data discovery", icon: "keyword.svg", hover: "keyword_hover.svg"},
+        {value: "Data movement", icon: "database.svg", hover: "database_hover.svg"},
+        {value: "Data registration", icon: "cloud_server.svg", hover: "cloud_server_hover.svg"},
+        {value: "Data storage", icon: "database_security.svg", hover: "database_security_hover.svg"}
     ];
     private services: Service[];
 
