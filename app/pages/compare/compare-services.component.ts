76ea5e7aa0a2153cf84ca0d5201447c07e895a10
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-11-29 11:36:40 +0200

    +vocabs in compare

--- app/pages/compare/compare-services.component.ts
+++ app/pages/compare/compare-services.component.ts
@@ -4,6 +4,7 @@
 import {Component, OnDestroy, OnInit} from "@angular/core";
 import {FormBuilder, FormGroup} from "@angular/forms";
 import {ActivatedRoute} from "@angular/router";
+import {Observable} from "rxjs/Observable";
 import {Subscription} from "rxjs/Subscription";
 import {Service} from "../../domain/eic-model";
 import {SearchQuery} from "../../domain/search-query";
@@ -22,9 +23,10 @@ export class CompareServicesComponent implements OnInit, OnDestroy {
     searchForm: FormGroup;
     public services: Service[] = [];
     public errorMessage: string;
-    private sub: Subscription;
     providers: any;
     nologo: URL = new URL("http://fvtelibrary.com/img/user/NoLogo.png");
+    vocabularies: any;
+    private sub: Subscription;
 
     constructor(fb: FormBuilder, private route: ActivatedRoute, private router: NavigationService,
                 private resourceService: ResourceService, private authenticationService: AuthenticationService,
@@ -33,8 +35,12 @@ export class CompareServicesComponent implements OnInit, OnDestroy {
     }
 
     ngOnInit() {
-        this.resourceService.getProviders().subscribe(providers => {
-            this.providers = providers;
+        Observable.zip(
+            this.resourceService.getProviders(),
+            this.resourceService.getVocabularies()
+        ).subscribe(suc => {
+            this.providers = suc[0];
+            this.vocabularies = this.transformVocabularies(suc[1]);
             this.sub = this.route.params.subscribe(params => {
                 let ids = (params.services || "").split(",");
                 if (ids.length > 1) {
@@ -50,6 +56,18 @@ export class CompareServicesComponent implements OnInit, OnDestroy {
 
     ngOnDestroy() {
         this.sub.unsubscribe();
+    }
+
+    transformVocabularies(vocabularies) {
+        let ret = {};
+        Object.entries(vocabularies).forEach(([key, value]) => {
+            let item = {};
+            item[key] = String(value.name);
+            let prefix = value.type;
+            ret[prefix] = ret[prefix] || {};
+            Object.assign(ret[prefix], item);
+        });
+        return ret;
     }
 
     onSubmit(searchValue: SearchQuery) {
