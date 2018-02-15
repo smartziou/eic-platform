a6fd369be647eccee2323854662dbdfa57445d9a
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-02-15 14:48:50 +0200

    +developers general info

--- app/pages/support/developers/developers.component.ts
+++ app/pages/support/developers/developers.component.ts
@@ -2,22 +2,19 @@
  * Created by pgl on 12/12/17.
  */
 
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 
 @Component({
     selector: "developers",
     templateUrl: "./developers.component.html",
     styleUrls: ["./developers.component.css"]
 })
-export class DevelopersComponent {
+export class DevelopersComponent implements OnInit {
 
-    //use this for now
-    getUI() {
-        return `${process.env.API_ENDPOINT}/swagger-ui.html`;
+    constructor() {
     }
 
-    //this needs consuming code/libraries; too much effort, not agile enough
-    getJSON() {
-        return `${process.env.API_ENDPOINT}/v2/api-docs`;
+    ngOnInit() {
+
     }
 }
