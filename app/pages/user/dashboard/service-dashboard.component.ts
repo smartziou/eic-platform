75a80daa70d0437133cb2576311684f060e3fbc0
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-02-22 14:23:14 +0200

    beaut

--- app/pages/user/dashboard/service-dashboard.component.ts
+++ app/pages/user/dashboard/service-dashboard.component.ts
@@ -1,16 +1,14 @@
 /**
  * Created by stefania on 1/9/18.
  */
-import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, Router } from "@angular/router";
-import { AuthenticationService } from "../../../services/authentication.service";
-import { ResourceService } from "../../../services/resource.service";
-import { UserService } from "../../../services/user.service";
-import { NavigationService } from "../../../services/navigation.service";
-import { URLParameter } from "../../../domain/url-parameter";
-import { Service } from "../../../domain/eic-model";
-import { SearchResults } from "../../../domain/search-results";
-import { Subscription } from "rxjs/Subscription";
+import {Component, OnInit} from "@angular/core";
+import {ActivatedRoute} from "@angular/router";
+import {Subscription} from "rxjs/Subscription";
+import {Service} from "../../../domain/eic-model";
+import {AuthenticationService} from "../../../services/authentication.service";
+import {NavigationService} from "../../../services/navigation.service";
+import {ResourceService} from "../../../services/resource.service";
+import {UserService} from "../../../services/user.service";
 
 @Component({
     selector: "service-dashboard",
@@ -18,7 +16,6 @@ import { Subscription } from "rxjs/Subscription";
     styleUrls: ["./service-dashboard.component.css"]
 })
 export class ServiceDashboardComponent implements OnInit {
-
     public service: Service;
     public errorMessage: string;
     private sub: Subscription;
@@ -29,10 +26,7 @@ export class ServiceDashboardComponent implements OnInit {
 
     ngOnInit() {
         this.sub = this.route.params.subscribe(params => {
-            this.resourceService.getService(id).subscribe(
-                service => this.service = service,
-                error => this.handleError(<any>error));
-            let id = params["id"];
+            this.resourceService.getService(params["id"]).subscribe(service => this.service = service, error => this.handleError(<any>error));
         });
     }
 
@@ -41,6 +35,6 @@ export class ServiceDashboardComponent implements OnInit {
     }
 
     handleError(error) {
-        this.errorMessage = 'System error retrieving service (Server responded: ' + error + ')';
+        this.errorMessage = "System error retrieving service (Server responded: " + error + ")";
     }
 }
