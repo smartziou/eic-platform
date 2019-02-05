9f07b3141ce2c88658088508c297e53d207caa63
Konstantinos Spyrou <spyroukon@gmail.com>
2019-02-05 12:15:52 +0200

    changed deployment method, added matomo variables

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,14 @@
 FROM nginx:alpine
 
-COPY nginx.conf /etc/nginx/nginx.conf
-COPY index.html /usr/share/nginx/html/
+COPY nginx.conf /etc/nginx/nginx.conf.tmpl
+COPY index.html /usr/share/nginx/html/index.html.tmpl
+COPY env_variables.sh /usr/share/nginx/
 COPY dist/ /usr/share/nginx/html/dist
 COPY js/ /usr/share/nginx/html/js
 COPY css/ /usr/share/nginx/html/css
 COPY imgs/ /usr/share/nginx/html/imgs
 COPY warp/	/usr/share/nginx/html/warp
 COPY assets/ /usr/share/nginx/html/assets
-COPY env_variables.sh /usr/share/nginx/
 RUN apk update && apk add bash
-ENTRYPOINT ["/bin/bash", "/usr/share/nginx/env_variables.sh", "/usr/share/nginx/html/dist"]
+ENTRYPOINT ["/bin/bash", "/usr/share/nginx/env_variables.sh"]
 EXPOSE 80
--- env_variables.sh
+++ env_variables.sh
@@ -1,29 +1,6 @@
 #!/bin/bash
 
-IFS=$'\n'
-DIST=${1:-.}
-
-function replace() {
-    sed  -i  "s|$1=\"[-/_:a-zA-Z0-9]*\"|$1=\"$2\"|g" ${DIST}/main.bundle.js
-}
-
-function sortVars() {
-    printenv \
-    | grep PLATFORM \
-    | awk '{v=$0; gsub(/=.*/,"",v) ;printf "%d %s\n", length(v), $0}' \
-    | sort -n \
-    | cut -d' ' -f2-
-}
-function replaceVariables() {
-    PLATFORM=$(sortVars)
-    for i in ${PLATFORM}; do
-        TEMP=${i#PLATFORM_}
-        varName=${TEMP%=*}
-        varValue=${TEMP#*=}
-        replace ${varName} ${varValue}
-    done
-}
-
-replaceVariables
+envsubst '${MATOMO_HOST} ${MATOMO_PORT} ${MATOMO_SITE_ID}' < /usr/share/nginx/html/index.html.tmpl > /usr/share/nginx/html/index.html &&
+envsubst '${PLATFORM_API_ENDPOINT}' < /etc/nginx/nginx.conf.tmpl > /etc/nginx/nginx.conf &&
 
 nginx -g "daemon off;"
