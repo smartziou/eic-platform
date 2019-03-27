95fa80281355fd7e4987e3fa1be3856d5812c4d0
Konstantinos Spyrou <spyroukon@gmail.com>
2019-03-27 13:23:40 +0200

    modified piwik script

--- env_variables.sh
+++ env_variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-envsubst '${MATOMO_HOST} ${MATOMO_PORT} ${MATOMO_SITE_ID}' < /usr/share/nginx/html/index.html.tmpl > /usr/share/nginx/html/index.html &&
+envsubst '${MATOMO_HOST} ${MATOMO_SITE_ID}' < /usr/share/nginx/html/index.html.tmpl > /usr/share/nginx/html/index.html &&
 envsubst '${PLATFORM_API_ENDPOINT}' < /etc/nginx/nginx.conf.tmpl > /etc/nginx/nginx.conf &&
 
 nginx -g "daemon off;"
