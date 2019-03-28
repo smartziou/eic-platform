2ce43b0b18e1ed61413a0b0fe7081e7f219a3ff5
Konstantinos Spyrou <spyroukon@gmail.com>
2019-03-28 16:50:41 +0200

    minor fix

--- env_variables.sh
+++ env_variables.sh
@@ -3,4 +3,4 @@
 envsubst '${MATOMO_HOST} ${MATOMO_SITE_ID}' < /usr/share/nginx/html/index.html.tmpl > /usr/share/nginx/html/index.html &&
 envsubst '${PLATFORM_API_ENDPOINT}' < /etc/nginx/nginx.conf.tmpl > /etc/nginx/nginx.conf &&
 
-nginx -g "daemon off;"
+nginx -s reload;
